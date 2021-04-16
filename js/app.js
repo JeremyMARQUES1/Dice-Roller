8eabb1b347b3146536b3bc37dce8986cf939598f
Etudiant O'clock <jeremy.marques@hotmail.fr>
2021-04-16 19:27:31 +0200

    regroupement des fonctions en objet dice

--- js/app.js
+++ js/app.js
@@ -1,37 +1,40 @@
-var dice = document.createElement('div');
+var diceDiv = document.createElement('div');
 
-dice.classList.add('dice');
+diceDiv.classList.add('dice');
 
 var player = document.getElementById('player');
 
-player.appendChild(dice);
+player.appendChild(diceDiv);
 
-function diceRoll(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(randomNumber);
-    return randomNumber;
-}
-
-function dice6() {
-    var diceResult = diceRoll(1, 6);
-    if (diceResult === 6) {
-        dice.style.backgroundPosition = "100%";
-    } else if (diceResult === 5) {
-        dice.style.backgroundPosition = "80%";
-    } else if (diceResult === 4) {
-        dice.style.backgroundPosition = "60%";
-    } else if (diceResult === 3) {
-        dice.style.backgroundPosition = "40%";
-    } else if (diceResult === 2) {
-        dice.style.backgroundPosition = "20%";
+dice = {
+    min: 1,
+    max: 6,
+    getRandomNumber: function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(randomNumber);
+        return randomNumber;
+    },
+    diceRoll: function() {
+        var diceResult = dice.getRandomNumber(dice.min, dice.max);
+        if (diceResult === dice.min) {
+            diceDiv.style.backgroundPosition = "0%";
+        } else if (diceResult === dice.min+1) {
+            diceDiv.style.backgroundPosition = "20%";
+        } else if (diceResult === dice.min+2) {
+            diceDiv.style.backgroundPosition = "40%";
+        } else if (diceResult === dice.min+3) {
+            diceDiv.style.backgroundPosition = "60%";
+        } else if (diceResult === dice.min+4) {
+            diceDiv.style.backgroundPosition = "80%";
+        } else {
+            diceDiv.style.backgroundPosition = "100%";
+        }
     }
 }
 
-
-var question = parseInt(prompt('Combien de dés souhaitez-vous lancer?'), 10);
-for(var compteur = 0; compteur<question; compteur++) {
-    dice6();
+var diceNumber = parseInt(prompt('Combien de dés souhaitez-vous lancer?'), 10);
+for (var count = 0; count < diceNumber; count++) {
+    dice.diceRoll();
 }
-
